29fdb294c21b299b2f3182d1e2ad85651cf635a5
Thomas G <64267405+ThomasGhila@users.noreply.github.com>
2021-11-08 18:03:48 +0100

    Add files via upload

--- second-sketch.js
+++ second-sketch.js
@@ -1,9 +1,7 @@
 const urlString = window.location.href;
 let url = new URL(urlString);
 
-console.log(url);
-
-const phrase = "Going fast won't keep you here!";
+const phrase = "Clicking the mouse won't keep you here!";
 const words = phrase.split(" ");
 let allButtons;
 let singleButton;
@@ -13,23 +11,27 @@ let help
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background("powderblue");
+ 
+  background("hsl(160, 100%, 50%)");
 
   words.forEach((word) => {
     createElement("button", word);
   });
+
+  allButtons = selectAll("button");
 
   help = createElement("help", "?");
   help.style(
     "position:absolute; border: 0px;background-color:transparent;color:#fff;text-decoration:none;padding:16px 31px;text-shadow:0px 1px 10px #000;font-size:45px;"
   );
   help.position(windowWidth-150, 40);
-
-  allButtons = selectAll("button");
 }
 
 function draw() {
-  
+ 
+  // textAlign(CENTER);
+  // textSize(20);
+  // text(frameCount, width / 2, height / 2);
 
   help.mouseOver(callHelp);
 
@@ -43,18 +45,15 @@ function draw() {
       "position:absolute; border: 0px;background-color:transparent;color:#fff;text-decoration:none;padding:16px 31px;text-shadow:0px 1px 10px #000;font-size:25px;"
     );
     singleButton.position(x, y);
+    
+    
   });
+}
 
-  let distance = dist(mouseX, mouseY, pmouseX, pmouseY);
-  let mappedDistance = map(distance, 0, width, 1, 50);
+function mouseClicked() {
+  //window.open("https://www.thomasghilardotti.com/", "_self");
 
-  stroke("WHITE")
-  strokeWeight(mappedDistance);
-  line(pmouseX, pmouseY, mouseX, mouseY);
-
-  if(mappedDistance > 15) {
-    window.open(url + "second-page.html", "_self");
-  }
+  window.open("http://127.0.0.1:5500/", "_self");
 }
 
 function callHelp() {
@@ -62,7 +61,7 @@ function callHelp() {
   noStroke();
   fill("WHITE");
   rect(windowWidth/2,windowHeight/2, 600, 150, 20)
-  fill("powderblue")
+  fill("hsl(160, 100%, 50%)")
   textAlign(CENTER);
-  text("ADVICE: Try getting your line thicker ;)", windowWidth/2, windowHeight/2);
+  text("ADVICE: Try clicking your mouse ;)", windowWidth/2, windowHeight/2);
 }
